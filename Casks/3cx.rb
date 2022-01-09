0453586e71870ba4cb1e2e7574962b3b6e5a963f
Karl Watkins <44941536+kwtns@users.noreply.github.com>
2022-01-09 12:09:30 +0000

    Add 3CX

---
+++ Casks/3cx.rb
@@ -0,0 +1,11 @@
+cask "3cx" do
+  version "16.0.1.0"
+  sha256 "06d65331e2441f0b03d0b8e96f1aa9571efc527afae4d726f098ca5a6c81a75c"
+
+  url "https://downloads-global.3cx.com/downloads/3CXPhoneforMac16.dmg"
+  name "3CX Phone"
+  desc "Desktop application for 3CX softphone"
+  homepage "https://3cx.com"
+
+  app "3CXPhone.app"
+end
